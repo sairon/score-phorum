c97fb27351e551d8ded29d91b4cde7035483a7ff
Jan Čermák <jan.cermak@nic.cz>
2015-09-18 18:38:05 +0200

    admin: classes for user admin

--- phorum/admin.py
+++ phorum/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.forms import UserChangeForm as DefaultUserChangeForm
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class UserChangeForm(DefaultUserChangeForm):
+    class Meta(DefaultUserChangeForm.Meta):
+        model = User
+
+
+class UserAdmin(DefaultUserAdmin):
+    form = UserChangeForm
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'nickname', 'password1', 'password2'),
+        }),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'nickname', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(User, UserAdmin)

7f0eb6cfcaf49972c7f8173abc380cb8f700e180
Jan Čermák <jan.cermak@nic.cz>
2015-12-02 20:43:29 +0100

    settings: allow beta.scorephorum.cz host

--- score/settings/production.py
+++ score/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["www.scorephorum.cz"]
+ALLOWED_HOSTS = ["www.scorephorum.cz", "beta.scorephorum.cz"]
 
 ADMINS = (
     ("Jan Cermak", "sairon@sairon.cz"),

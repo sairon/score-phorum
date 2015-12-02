6399f11d4a7b90e4735983968f880d3804e805bb
Jan Čermák <jan.cermak@nic.cz>
2015-12-02 22:09:29 +0100

    settings: use template cached loader in production

--- score/settings/production.py
+++ score/settings/production.py
@@ -2,6 +2,13 @@ from .base import *
 
 DEBUG = False
 
+del TEMPLATES[0]['APP_DIRS']
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ])]
+
 ALLOWED_HOSTS = ["www.scorephorum.cz", "beta.scorephorum.cz"]
 
 ADMINS = (

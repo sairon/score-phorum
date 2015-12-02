b20136e684ea4553b1f7fa768b6de88b2e80f93e
Jan Čermák <jan.cermak@nic.cz>
2015-12-02 22:46:28 +0100

    add forgotten nl2br filter for message text

--- phorum/templatetags/score_tags.py
+++ phorum/templatetags/score_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -12,3 +13,8 @@ def new_posts(room, visits):
 @register.filter
 def can_be_deleted_by(message, user):
     return message.can_be_deleted_by(user)
+
+
+@register.filter
+def nl2br(text):
+    return mark_safe(text.strip().replace("\n", "<br>"))

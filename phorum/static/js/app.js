9b22527e409ed1dea80faddc28477534d92a462b
Jan Čermák <jan.cermak@nic.cz>
2015-12-20 16:52:34 +0100

    js: consistently use single quotes

--- phorum/static/js/app.js
+++ phorum/static/js/app.js
@@ -4,7 +4,7 @@
     var recipientInput = $('#id_recipient'),
       threadInput = $('#id_thread');
     if (recipientInput) {
-      recipientInput.addClass("reply");
+      recipientInput.addClass('reply');
       recipientInput.val(rootMessage.data('author'));
       threadInput.val(rootMessage.data('thread-id'));
       window.scrollTo(0, 0);
@@ -13,13 +13,13 @@
   });
 
   $('#id_recipient').on('change paste input', function(e) {
-    $("#id_thread").val("");
-    $("#id_recipient").removeClass("reply");
+    $('#id_thread').val('');
+    $('#id_recipient').removeClass('reply');
   });
 
   $('#id_to_inbox').on('change', function(e) {
-    $("#id_thread").val("");
-    $("#id_recipient").removeClass("reply");
+    $('#id_thread').val('');
+    $('#id_recipient').removeClass('reply');
   });
 
   $('.delete-link a, .delete-link-mobile').click(function(e) {

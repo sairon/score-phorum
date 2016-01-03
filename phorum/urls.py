f0a13315e2df154b2182d39ca7f19c9cfd2ddf19
Jan Čermák <jan.cermak@nic.cz>
2016-01-03 19:56:31 +0100

    urls: put phorum urls to phorum.urls module

---
+++ phorum/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns(
+    "phorum.views",
+    url(r'^$', 'room_list', name="home"),
+    url(r'^inbox$', 'inbox', name="inbox"),
+    url(r'^inbox/new-message$', 'inbox_send', name="inbox_send"),
+    url(r'^room/new$', 'room_new', name="room_new"),
+    url(r'^room/(?P<room_slug>.+)/$', 'room_view', name="room_view"),
+    url(r'^room/(?P<room_slug>.+)/password$', 'room_password_prompt', name="room_password_prompt"),
+    url(r'^room/(?P<room_slug>.+)/mark-unread$', 'room_mark_unread', name="room_mark_unread"),
+    url(r'^room/(?P<room_slug>.+)/edit$', 'room_edit', name="room_edit"),
+    url(r'^room/(?P<room_slug>.+)/new-message$', 'message_send', name="message_send"),
+    url(r'^login$', 'login', name="login"),
+    url(r'^logout$', 'logout', name="logout"),
+    url(r'^user/new', 'user_new', name="user_new"),
+    url(r'^user/edit', 'user_edit', name="user_edit"),
+    url(r'^users/', 'users', name="users"),
+    url(r'^message/(?P<message_id>\d+)/delete', 'message_delete', name="message_delete"),
+)
--- score/urls.py
+++ score/urls.py
@@ -20,19 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'phorum.views.room_list', name="home"),
-    url(r'^inbox$', 'phorum.views.inbox', name="inbox"),
-    url(r'^inbox/new-message$', 'phorum.views.inbox_send', name="inbox_send"),
-    url(r'^room/new$', 'phorum.views.room_new', name="room_new"),
-    url(r'^room/(?P<room_slug>.+)/$', 'phorum.views.room_view', name="room_view"),
-    url(r'^room/(?P<room_slug>.+)/password$', 'phorum.views.room_password_prompt', name="room_password_prompt"),
-    url(r'^room/(?P<room_slug>.+)/mark-unread$', 'phorum.views.room_mark_unread', name="room_mark_unread"),
-    url(r'^room/(?P<room_slug>.+)/edit$', 'phorum.views.room_edit', name="room_edit"),
-    url(r'^room/(?P<room_slug>.+)/new-message$', 'phorum.views.message_send', name="message_send"),
-    url(r'^login$', 'phorum.views.login', name="login"),
-    url(r'^logout$', 'phorum.views.logout', name="logout"),
-    url(r'^user/new', 'phorum.views.user_new', name="user_new"),
-    url(r'^user/edit', 'phorum.views.user_edit', name="user_edit"),
-    url(r'^users/', 'phorum.views.users', name="users"),
-    url(r'^message/(?P<message_id>\d+)/delete', 'phorum.views.message_delete', name="message_delete"),
+    url(r'^', include("phorum.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
